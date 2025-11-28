53f82f1effb3aab53ffa43d3fe5cd3a41f864868
Matt-Holeman <mholeman@caci.co.uk>
2025-11-28 15:54:14 +0000

    Add template day

---
+++ Day xx/PartOne.py
@@ -0,0 +1,13 @@
+import time
+
+def main():
+    start = time.time()
+    solve()
+    end = time.time()
+    print(f"Took: {round((end-start) * 10**3)}ms")
+
+def solve():
+    return
+
+if __name__ == "__main__":
+    main()
